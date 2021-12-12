8768d6d81e01d6e75849e0f5d29125991a1a6cc8
ViniciusAguiar40 <74572919+ViniciusAguiar40@users.noreply.github.com>
2021-12-12 13:24:47 -0300

    Adicionando 2a lei de controle retroacao de estados

---
+++ Retroacao_de_estados_2a_Lei.m
@@ -0,0 +1,24 @@
+% Segunda Lei do Controle por Retroação de Estados
+%
+% Metodo de Ackerman
+
+% Funcao de transferencia
+% NUM = 1370;
+% DEN = [1 50 0]; % há um polo na origem
+% se nao for dado as matrizes
+% [a,b,c,d] = tf2ss(NUM, DEN);
+
+% matrizes
+a=[[0 1]; [0 -50]];
+b=[0;1370];
+c=eye(2); % matriz identidade
+d=0;
+
+J = [-4.5 -180]; % polos desejados
+
+K = acker(a,b,J);
+K1 = K(1)
+K2 = K(2)
+
+% Rodar o simulink e depois:
+plot(tempo,ref,tempo,x1,tempo,x2)
